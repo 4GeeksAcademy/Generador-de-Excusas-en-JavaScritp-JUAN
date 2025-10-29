44a24c8ec492718fa6440d1d704f5bc2bc998170
juanMasis777 <juanmasisg@gmail.com>
2025-10-29 00:02:05 +0000

    Project done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuseElement = document.querySelector('#excuse')
+  function GetRandomData(array) {
+    const RandomData = Math.floor(Math.random()* array.length);
+    return array[RandomData]
+  }
+  
+  const who = ['Mi amigo', 'Mi Amiga', 'Mi Hermano', 'Mi Prima'];
+  const RandomWho = GetRandomData(who)
+  const action = ['nadó',  'comió', 'mazticó', 'durmió'];
+  const RandomAction = GetRandomData(action)
+  const what = ['mi casa', 'mi carro', 'mi barco'];
+  const RandomWhat = GetRandomData(what)
+  const when = ['en el concierto', 'cuando estaba comiendo', 'mientras bailaba', 'durante mi siesta', 'cuando estaba leyendo'];
+  const RandomWhen = GetRandomData(when)
+
+  const excuse = RandomWho + " " + RandomAction + " " + RandomWhat + " " + RandomWhen
+  excuseElement.innerText = excuse 
 };
